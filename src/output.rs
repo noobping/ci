cf10766937d518d83105ded483352efd23c637d0
nick <noobping@users.noreply.github.com>
2026-04-24 15:30:35 +0200

    logs

--- src/output.rs
+++ src/output.rs
@@ -1,4 +1,13 @@
+use std::io::IsTerminal;
+use std::sync::Once;
+
+use tracing::Level;
+use tracing_subscriber::fmt::writer::MakeWriterExt;
+
 use crate::cli::GlobalOptions;
+use crate::config::ColorWhen;
+
+static INIT_TRACING: Once = Once::new();
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Verbosity {
@@ -15,6 +24,8 @@ pub struct Output {
 
 impl Output {
     pub fn from_globals(global: &GlobalOptions) -> Self {
+        init_tracing(global);
+
         let verbosity = if global.silent {
             Verbosity::Silent
         } else if global.quiet {
@@ -34,23 +45,52 @@ impl Output {
 
     pub fn info(&self, message: impl AsRef<str>) {
         if !matches!(self.verbosity, Verbosity::Quiet | Verbosity::Silent) {
-            println!("{}", message.as_ref());
+            tracing::info!("{}", message.as_ref());
         }
     }
 
     pub fn warn(&self, message: impl AsRef<str>) {
         if !matches!(self.verbosity, Verbosity::Silent) {
-            eprintln!("ci: warning: {}", message.as_ref());
+            tracing::warn!("{}", message.as_ref());
         }
     }
 
     pub fn error(&self, message: impl AsRef<str>) {
-        eprintln!("ci: error: {}", message.as_ref());
+        tracing::error!("{}", message.as_ref());
     }
 
     pub fn verbose(&self, message: impl AsRef<str>) {
-        if matches!(self.verbosity, Verbosity::Verbose(_)) {
-            eprintln!("ci: {}", message.as_ref());
+        if let Verbosity::Verbose(level) = self.verbosity {
+            if level > 1 {
+                tracing::trace!("{}", message.as_ref());
+            } else {
+                tracing::debug!("{}", message.as_ref());
+            }
         }
+    }
+}
+
+fn init_tracing(global: &GlobalOptions) {
+    INIT_TRACING.call_once(|| {
+        let writer = std::io::stderr
+            .with_max_level(Level::WARN)
+            .or_else(std::io::stdout);
+        let ansi = color_enabled(global.color);
+
+        let _ = tracing_subscriber::fmt()
+            .compact()
+            .without_time()
+            .with_target(false)
+            .with_ansi(ansi)
+            .with_writer(writer)
+            .try_init();
+    });
+}
+
+fn color_enabled(color: ColorWhen) -> bool {
+    match color {
+        ColorWhen::Always => true,
+        ColorWhen::Never => false,
+        ColorWhen::Auto => std::io::stdout().is_terminal() || std::io::stderr().is_terminal(),
     }
 }
